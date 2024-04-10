b335eaea0fcc99884c178d8d19b6e71c3d2c243f
Nick Jasso <nick.nite09@gmail.com>
2024-04-09 20:37:07 -0700

    added baziingaaz and monsewubz channels

--- src/bot.js
+++ src/bot.js
@@ -7,7 +7,7 @@ import { getDatabase, ref, update, set, get } from 'firebase/database';
 
 const username = 'pc_ii';
 const oauthToken = process.env.OAUTHTOKEN;
-const channels = [ 'pc_ii' ];
+const channels = [ 'pc_ii', 'baziingaaz', 'monsewubz' ];
 const spamCooldown = 5000; // in ms
 const redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
 const blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
